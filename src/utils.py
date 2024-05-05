af4d01311ab62192df79b1895ecefbe87d535b97
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-05-05 21:32:29 +0300

    add get_vacancies_by_employer function

--- src/utils.py
+++ src/utils.py
@@ -35,3 +35,29 @@ def get_employers_data(employers_id: list) -> list[dict]:
         employers_data.append(search_result)
 
     return employers_data
+
+
+def get_vacancies_data_by_employer(employers_id) -> list[dict]:
+    """Получение данных о вакансиях конкретного работодателя"""
+
+    headers = {'User-Agent': 'HH-User-Agent'}
+    params = {'employer_id': '', 'page': 0, 'per_page': 100}
+    url = 'https://api.hh.ru/vacancies'
+
+    vacancies_by_employer = []
+
+    for id_emp in employers_id:
+        params['employer_id'] = id_emp
+        params['page'] = 0
+        vacancies_to_add = []
+        pages_limit = requests.get(url, params, headers=headers).json().get("pages")
+
+        while params.get('page') != pages_limit:
+            response = requests.get(url, params, headers=headers)
+            vacancies = response.json().get('items', [])
+            vacancies_to_add.extend(vacancies)
+            params['page'] += 1
+
+        vacancies_by_employer.append({id_emp: vacancies_to_add})
+
+    return vacancies_by_employer
