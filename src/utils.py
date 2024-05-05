f2c9a1901b80068d6c20207c63b4433a7c9c7674
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-05-05 21:31:23 +0300

    add get_employers_data function

--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,19 @@ def get_employers_id(employers: list[str]) -> list:
             employers_id.append(search_result[0].get('id', ''))
 
     return employers_id
+
+
+def get_employers_data(employers_id: list) -> list[dict]:
+    """Получение данных о работодателях"""
+
+    headers = {'User-Agent': 'HH-User-Agent'}
+
+    employers_data = []
+    for emp_id in employers_id:
+        employer_id = emp_id
+        url = f'https://api.hh.ru/employers/{employer_id}'
+        response = requests.get(url, headers=headers)
+        search_result = response.json()
+        employers_data.append(search_result)
+
+    return employers_data
