fb0d06240eef98c5cd79cb718f4b09f1ff7d9c35
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-05-07 00:06:49 +0300

    create DBManager class, add __init__, __del__ and get_companies_and_vacancies_count method

---
+++ src/dbmanager_class.py
@@ -0,0 +1,42 @@
+import psycopg2
+
+
+class DBManager:
+    """Класс для работы с базой данной PostgreSQL"""
+
+    def __init__(self, database_name: str, params: dict):
+        """Инициализатор класса"""
+        self.database_name = database_name
+        self.params = params
+        self.conn = psycopg2.connect(dbname=database_name, **params)
+
+    def __del__(self):
+        """Метод для закрытия соединения с базой данных перед удалением экземпляра"""
+        self.conn.close()
+
+    def get_companies_and_vacancies_count(self):
+        """Получает список всех компаний и количество вакансий у каждой компании"""
+
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                SELECT name, open_vacancies FROM public.employers
+                ORDER BY open_vacancies DESC
+            """)
+            return cur.fetchall()
+
+    def get_all_vacancies(self):
+        """Получает список всех вакансий с указанием названия компании,
+            названия вакансии и зарплаты и ссылки на вакансию"""
+        pass
+
+    def get_avg_salary(self):
+        """Получает среднюю зарплату по вакансиям"""
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
+        pass
+
+    def get_vacancies_with_keyword(self):
+        """Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python"""
+        pass
